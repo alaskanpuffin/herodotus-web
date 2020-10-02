38bcfe105333f8dc975d8e171b6cac877878551d
Caleb Hurley <calebhurley@protonmail.com>
2020-10-01 21:34:33 -0800

    Add Import/Export

--- src/views/GeneralSettings.vue
+++ src/views/GeneralSettings.vue
@@ -9,6 +9,24 @@
       >Log Out All Current Sessions</a
     >
 
+    <div class="divider"></div>
+
+    <div class="row">
+      <div class="col-sm-6">
+        <a class="btn" @click="exportArticles"
+          ><span v-if="exportLoading == false">Export Articles</span
+          ><i v-else class="fa fa-spinner fa-pulse"></i
+        ></a>
+      </div>
+      <div class="col-sm-6">
+        <a class="btn" @click="importArticles"
+          ><span v-if="importLoading == false">Import Articles</span
+          ><i v-else class="fa fa-spinner fa-pulse"></i
+        ></a>
+        <input ref="importinput" type="file" style="display: none" />
+      </div>
+    </div>
+
     <p id="version">
       Frontend Version: {{ $store.state.version }} - Backend Version:
       <span v-if="versionLoading == false">{{ backendVersion }}</span
@@ -21,6 +39,7 @@
 import axios from "axios";
 import toastr from "toastr";
 import Configuration from "@/configuration.js";
+import FileDownload from "js-file-download";
 
 const apiRoot = Configuration.value("apiRoot");
 
@@ -31,6 +50,9 @@ export default {
       indexloading: false,
       versionLoading: true,
       backendVersion: "",
+      downloadUrl: "",
+      exportLoading: false,
+      importLoading: false,
     };
   },
   mounted() {
@@ -45,6 +67,31 @@ export default {
       .then(() => {
         this.versionLoading = false;
       });
+
+    this.$refs.importinput.addEventListener("change", (event) => {
+      var formData = new FormData();
+      this.importLoading = true;
+      formData.append("content", event.target.files[0]);
+      var config = {
+        headers: {
+          Authorization: "Token " + this.$cookies.get("token"),
+          'Content-Type': 'multipart/form-data',
+        },
+      };
+      axios
+        .post(`${apiRoot}/import/`, formData, config)
+        .then(() => {
+          toastr.success("The articles have been imported.");
+        })
+        .catch((error) => {
+          console.log(error);
+          toastr.error("An error has occured.");
+        })
+        .then(() => {
+          event.target.value = '';
+          this.importLoading = false;
+        });
+    });
   },
   methods: {
     logoutAllSessions: function () {
@@ -92,6 +139,34 @@ export default {
           });
       }
     },
+    exportArticles: function () {
+      if (this.exportLoading == false) {
+        this.exportLoading = true;
+        var config = {
+          headers: {
+            Authorization: "Token " + this.$cookies.get("token"),
+          },
+          responseType: 'text',
+          transformResponse: [data => data],
+        };
+        axios
+          .get(`${apiRoot}/export/`, config)
+          .then((response) => {
+            console.log(response.data)
+            FileDownload(response.data, "herodotus_export.json");
+          })
+          .catch((error) => {
+            console.log(error);
+            toastr.error("An error has occured.");
+          })
+          .then(() => {
+            this.exportLoading = false;
+          });
+      }
+    },
+    importArticles: function () {
+      this.$refs.importinput.click();
+    },
   },
 };
 </script>
@@ -104,10 +179,14 @@ export default {
   padding: 20px 30px;
   min-height: 300px;
   position: relative;
+  .divider {
+    height: 1px;
+    width: 100%;
+    margin-top: 30px;
+    background-color: rgb(238, 238, 238);
+  }
   #version {
-    position: absolute;
-    bottom: 20px;
-    left: 0;
+    margin-top: 40px;
     width: 100%;
     text-align: center;
   }
@@ -122,6 +201,12 @@ export default {
     margin-top: 30px;
     text-align: center;
     transition: 0.3s;
+    border: solid rgb(175, 175, 175) 2px;
+    color: rgb(85, 85, 85);
+    &:hover {
+      background-color: rgb(175, 175, 175);
+      color: #fff;
+    }
   }
   #logoutall {
     border: solid rgb(179, 74, 74) 2px;
